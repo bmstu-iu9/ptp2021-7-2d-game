cf1f5b13baa650abc1790b1358ae5577176257c4
p0rtale <petryaev_1984@mail.ru>
2021-08-23 13:24:56 +0300

    basic spell drawing system (without recognition)

--- client/src/game/interfaceScene.js
+++ client/src/game/interfaceScene.js
@@ -23,15 +23,51 @@ export class InterfaceScene extends Phaser.Scene {
         this.load.image('elementVapor', 'public/assets/textures/elementVapor.png');
         this.load.image('elementPoison', 'public/assets/textures/elementPoison.png');
 
+        this.load.image('particle', 'public/assets/textures/particle_red.png');
+
         this.load.json('inputdata', 'public/settings/input.json');
     }
 
     create() {
         this.elementsInterface = new ElementsInterface(this, 5, 960, 1040, 360, 80, 60, 60, 10, 'elementNull', 'interfaceBackground');
 
+        /* Particles for drawing
+
+            TODO: make particles more visible
+              (perhaps darkening the screen will solve this)
+
+        */
+
+        this.particles = this.add.particles('particle');
+
+        this.emitter = this.particles.createEmitter({
+            on: false,
+            speed: 100,
+            scale: { start: 1, end: 0 },
+            quantity: 2,
+            blendMode: 'ADD'
+        });
+
+        //---------------------------------------------------
+
         const data = this.cache.json.get('inputdata');
         for (const element in data['elements']) {
             this.addKeyboardEvent(element, data['elements'][element], 'down', 'addElement');
+        }
+    }
+
+    update() {
+        /* Basic drawing control
+
+            TODO: everything else from Issue #9
+            
+        */
+
+        if (this.input.mousePointer.isDown) {
+            this.emitter.start();
+            this.emitter.setPosition(this.input.mousePointer.x, this.input.mousePointer.y);
+        } else {
+            this.emitter.stop();
         }
     }
 
